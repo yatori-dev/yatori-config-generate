5c630d70f0850a76cb11167a3195d1cd82956450
Changbaiqi <changbaiqi@foxmail.com>
2025-05-27 19:46:05 +0800

    update: 更新

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  <YamlConfigEditor></YamlConfigEditor>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import YamlConfigEditor from './components/YamlConfigEditor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    YamlConfigEditor
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-
-createApp(App).mount('#app')
+import Antd from 'ant-design-vue'
+const app = createApp(App)
+app.use(Antd)
+app.mount('#app')
