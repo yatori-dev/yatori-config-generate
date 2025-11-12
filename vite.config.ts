8f25a158b81931fbb05ba1ce9b5fc6632bd81d63
Changbaiqi <changbaiqi@foxmail.com>
2025-11-13 01:19:22 +0800

    update: 尝试更改路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/yatori-config-generate/',
+  // base: '/yatori-config-generate/',
+  base: './',
   plugins: [vue()],
 })
